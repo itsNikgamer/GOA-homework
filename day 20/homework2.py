aed6a43c0d30ed97f20543bade6021cb08bdaa90
itsNikgamer <nikolozatat@gmail.com>
2024-09-09 20:06:42 +0400

    seventh commit

--- day 20/homework2.py
+++ day 20/homework2.py
@@ -1,7 +1,9 @@
-user_input=input("please input numbers 1-10")
+user_input=input("please input numbers 1-10:")
 if user_input:
     user_input=6
     print("მოიგეთ 100'000₾")
 elif user_input:
     user_input=2
     print("თქვენ მოიგეთ 10'000₾")
+else:
+    print("შენ წააგე")
