574f4560541f236836e9292a6d7023e8a1a57648
itsNikgamer <nikolozatat@gmail.com>
2024-09-14 11:28:37 +0400

    eighth commit

---
+++ day 22/homework1.py
@@ -0,0 +1,2 @@
+favourite_films=["avengers,lord of the rings, hobbit, shaqro da zaqro, harry potter"]
+print (favourite_films)
---
+++ day 22/homework3.py
@@ -0,0 +1,4 @@
+friend_list=["ანდრია,გიო,ბაჩო,ლუკა"]#მეტი არ დავწერე იმიტომ რომ ძაან დიდი არ ყოფილიყო
+hello=("გამარჯობა")
+for friend_list in range(hello):
+    print (friend_list)
