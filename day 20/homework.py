50c5a35dd99d1db6a28d657b116b4102dcdcfc5b
itsNikgamer <nikolozatat@gmail.com>
2024-09-14 11:27:29 +0400

    seventh commit

--- day 20/homework.py
+++ day 20/homework.py
@@ -1,6 +1,7 @@
 user_input=input("შეიყვანე რიცხვი:")
 if user_input == 0:
     print ("თქვენ შეიყვანეთ 0")
-elif user_input=
- print ("")
- 
+elif user_input==1:
+ print ("შენ შეიყვანე 1")
+else:
+   print ("მერავიცი რა შეიყვანე წინასწარმეტყველი კი არ ვარ")
--- day 20/homework1.py
+++ day 20/homework1.py
@@ -2,6 +2,3 @@ user_input=input
 if user_input:
     user_input<100
 print ("less than the other") 
-
-
-#ეს ვერ გავიგე ბოდიში
